58d082476afab2b5258f244ff9f6b37302b7185e
aziznemlaghi <aziznemlaghi20@gmail.com>
2020-11-26 11:28:12 +0100

    njnn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProduitsComponent } from './produits/produits.component';
 import { AddProduitComponent } from './add-produit/add-produit.component';
+
 const routes: Routes = [
-{path: "produits", component : ProduitsComponent},
-{path: "add-produit", component : AddProduitComponent},
-{ path: "", redirectTo: "produits", pathMatch: "full" }
+  { path: "produits", component : ProduitsComponent },
+  { path: "add-produit", component : AddProduitComponent },
+  { path: "", redirectTo:"produits",  pathMatch:"full" },
 ];
 
 @NgModule({
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -3,15 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css']
 })
 export class ProduitsComponent implements OnInit {
-  produits : string[];
-  constructor() {
+  produits:String[];
+  constructor() { 
     this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
-   }
+  }
 
   ngOnInit(): void {
-    
   }
 
 }
